c18df118e6ad22d55d292e6dcc44283f4eacdfb7
Khizar Wahab <Khizarwahab19@gmail.com>
2023-07-24 17:05:28 +0500

    Add changes

--- app/Http/Controllers/Admin/TaskController.php
+++ app/Http/Controllers/Admin/TaskController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Blade;
 
 class TaskController extends Controller
 {
@@ -56,8 +57,71 @@ class TaskController extends Controller
 
 
               $event->users()->sync($request->users);
+
+              $users = User::all();
+
+              $modalHTML = <<<'blade'
+                <x-primary-button id="editModalBtn{{ $task->id }}" class="hidden" x-data="" x-on:click.prevent="$dispatch('open-modal', 'editModal{{ $task->id }}')"></x-primary-button>
+
+                <x-modal name="editModal{{ $task->id }}" focusable>
+                    <div class="p-6">
+                        <div class="flex items-center justify-between mb-4">
+                            <h1 class="text-lg font-medium text-gray-900">
+                                Edit Task
+                            </h1>
+                            <x-danger-button type="button" onclick="deleteTask({{ $task->id }})">Delete</x-danger-button>
+                        </div>
+                        <div class="space-y-6">
+                            <div class="row">
+                                <div class="col-md-12 mb-3">
+                                    <label>Users</label>
+                                    <select class="block w-full js-example-basic-single" id="select2{{ $task->id }}" multiple name="user_id[]">
+                                        @foreach($users as $user)
+                                        <option value="{{ $user->id }}" {{ $task->users->contains($user)? 'selected': '' }}>{{ $user->name ?? '' }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+            
+                                <div class="col-md-12 mb-3">
+                                    <label for="inputPassword4" class="form-label">Task Description</label>
+                                    <textarea class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 block w-full" id="event_description" name="description">{{ $task->task_description }}</textarea>
+                                </div>
+            
+                                <div class="col-md-12 mb-3">
+                                    <label for="inputEmail4" class="form-label">Task Type</label>
+                                    <select name="task_type" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 block w-full" id="task_type">
+                                        <option value="">Select Task Type</option>
+                                        <option value="0" {{ $task->task_type == "0"? 'selected': '' }}>Daily</option>
+                                        <option value="1" {{ $task->task_type == "1"? 'selected': '' }}>Weekly</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-12 mb-3">
+                                    <label for="inputCity" class="form-label">Task Time</label>
+                                    <input type="time" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 block w-full" id="event_time" name="time" value="{{ $task->task_time }}">
+                                </div>
+                                <div class="col-md-12 mb-3">
+                                    <label for="inputCity" class="form-label">Task End Date</label>
+                                    <input type="date" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 block w-full" id="event_end_date" name="end_date" value="{{ $task->end_date }}">
+                                </div>
+                            </div>
+            
+                            <div class="mt-6 flex justify-end">
+                                <x-secondary-button x-on:click="$dispatch('close')" id="closeEditBtn{{ $task->id }}">
+                                    {{ __('Cancel') }}
+                                </x-secondary-button>
+            
+                                <x-primary-button type="button" class="ml-3" onclick="submitForm()">
+                                    Submit
+                                </x-primary-button>
+                            </div>
+                        </div>
+                    </div>
+                </x-modal>
+              blade;
+
+              $modalHTML = htmlspecialchars(Blade::render($modalHTML, ['task' => $event, 'users' => $users]), ENT_QUOTES, 'UTF-8', false);
  
-              return response()->json($event);
+              return response()->json(['event' => $event, 'modalHTML' => $modalHTML]);
              break;
   
            case 'update':
