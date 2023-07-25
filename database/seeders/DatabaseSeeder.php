d26d5617fbc93bcc80aaf1fe65f43a0180b0241f
Khizar Wahab <Khizarwahab19@gmail.com>
2023-07-25 19:29:08 +0500

    changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
@@ -20,16 +21,24 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        // $role = Role::create(['name' => 'admin']);
-        // $role = Role::create(['name' => 'user']);
+
+        if (! Role::where('name', 'admin')->first()) {
+            $role = Role::create(['name' => 'admin']);
+        }
+
+        if (! Role::where('name', 'user')->first()) {
+            $role = Role::create(['name' => 'user']);
+        }
 
         
-        $admin = User::create([
-            'name' => 'admin',
-            'email' => 'admin@gmail.com',
-            'password' => '123456'
-        ]);
-
-        $admin->assignRole('admin');
+        if (! User::where('email', 'admin@gmail.com')->first()) {
+            $admin = User::create([
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('123456')
+            ]);
+    
+            $admin->assignRole('admin');
+        }
     }
 }
